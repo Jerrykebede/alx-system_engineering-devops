8fa834a00b3e417b10c6c791158093a4db250bf3
JerryKebede <eyerusalem_ke@yahoo.com>
2024-06-29 03:33:10 -0700

    done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,5 @@
-# 1-install_flask.pp
-
-# Ensure Python 3.8.10 is installed (if not already managed separately)
-package { 'python3':
-  ensure => '3.8.10',
-}
-
-# Install Flask version 2.1.0 using pip3
-package { 'Flask':
+# Install an especific version of flask (2.1.0)
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
-  require  => Package['python3'],  # Ensure Python is installed first
+  provider => 'pip3'
 }
